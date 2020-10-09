fcd27ee67e227c4aca007ad6a0d0d9af485a3c4e
nikita <nikitazozoulenko@github.com>
2020-10-09 20:28:05 +0200

    tried starting item textures

--- src/blockRenderer.cpp
+++ src/blockRenderer.cpp
@@ -10,6 +10,7 @@ namespace fs = boost::filesystem;
 #include <array>
 #include <string>
 #include <vector>
+#include <iostream>
 
 BlockRenderer::BlockRenderer(Settings& settings) :
     settings(settings)
@@ -199,9 +200,39 @@ void BlockRenderer::setupBlockData()
 
 void BlockRenderer::createItemTextures()
 {
+    int size=256;
     for(auto& pair : blockIDMap){
-        std::string name = pair.first;
-        unsigned int blockID = pair.second;
-        itemTexMap[blockID] = blockID;
+        unsigned int& blockID = pair.second;
+
+        // framebuffer configuration
+        // -------------------------
+        unsigned int framebuffer;
+        glGenFramebuffers(1, &framebuffer);
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        // create a color attachment texture
+        unsigned int textureColorbuffer;
+        glGenTextures(1, &textureColorbuffer);
+        glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, size, size, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+        // create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+        unsigned int rbo;
+        glGenRenderbuffers(1, &rbo);
+        glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, size, size); // use a single renderbuffer object for both a depth AND stencil buffer.
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
+        // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+            std::cerr << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+
+        //render to texture
+
+
+        //done
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);   
+        glDeleteFramebuffers(1, &framebuffer);  
     }
 }
+
